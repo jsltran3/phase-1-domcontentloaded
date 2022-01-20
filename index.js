c7bed380eda4873648583e3310c5cf249e8bdcac
Joseph <jsltran@gmail.com>
2022-01-20 15:02:43 -0800

    commit tree 2c1a97edaa2904cfbf1e7b8196475e9ca16d38bc

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+// const headerTextChange = document.body.querySelector('#text').innerHTML = 'This is really cool!'
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.querySelector('#text').textContent = 'This is really cool!';
+});
